48370c71ab11f72bea288b741dc3a8ae3ef18dbf
Britney Agius <britneyagius@britneys-mbp.home>
2021-01-08 12:00:26 +0100

    Shreddar, WayPoints, Obstacles
    1. Shreddar
    2. Paths
    3. WayPoints
    4. WaveConfig Script
    5. Obstacles
    6. Obstacle Pathing

---
+++ 2D Car Game/Assets/Scripts/ObstaclePathing.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePathing : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float ObstacleSpeed = 2f;
+
+    //saves the waypoint in which we want to go
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the start position of the obstacle to the 1st WayPoint position
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void ObstacleMove()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            //set targetPosition to the next waypoint
+            Vector3 targetPosition = waypoints[waypointIndex].transform.position;
+            targetPosition.z = 0f; //Make sure the z axis = 0
+
+            float movementThisFrame = ObstacleSpeed * Time.deltaTime;
+
+            //move obstacle from current position to targetPosition, at movementThisFrame
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            //if obstacle reaches the first point
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        //if obstacle reaches last waypoint
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 2D Car Game/Assets/Scripts/Shreddar.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shreddar : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        print("Collision with " + otherObject.name);
+    }
+}
